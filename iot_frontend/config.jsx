7280ed28af94c6cf71c524118127a89dc8a3dab9
ellie25v <yelizarieva.ellie@gmail.com>
2025-01-28 18:34:29 +0100

    updated broker_url

--- iot_frontend/config.jsx
+++ iot_frontend/config.jsx
@@ -1,3 +1,4 @@
 export const API_URL = "https://backend.visiongrid.online";
 // export const BROKER_URL = "ws://3.15.141.7:8083/mqtt";
-export const BROKER_URL = "ws://local.visiongrid.online:8083/mqtt";
+// export const BROKER_URL = "wss://local.visiongrid.online:8084/mqtt";
+export const BROKER_URL = "wss://emqx1:8084/mqtt";
