3cadaedf93360eb34fc40b2789f035731b30b9bb
Kolia Palahniuk <nikolas.palahniuk@zealicsolutions.com>
2025-01-28 20:39:19 +0100

    fe: update connection port

--- iot_frontend/config.jsx
+++ iot_frontend/config.jsx
@@ -1,4 +1,4 @@
 export const API_URL = "https://backend.visiongrid.online";
 // export const BROKER_URL = "ws://3.15.141.7:8083/mqtt";
 // export const BROKER_URL = "wss://local.visiongrid.online:8084/mqtt";
-export const BROKER_URL = "wss://socket-mqtt.visiongrid.online/mqtt";
+export const BROKER_URL = "ws://socket-mqtt.visiongrid.online/mqtt";
